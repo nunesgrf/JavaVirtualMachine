fd9453334f05e968cf2d6617fa298a7c749a26e5
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-07 18:11:23 -0300

    Colocando classe e dividindo atributos

--- src/main/cpp/CpInfo.cpp
+++ src/main/cpp/CpInfo.cpp
@@ -6,51 +6,63 @@
 
 
 
-struct CpInfo {
+class CpInfo {
 
-    uint8_t tag;
-    union {
-        struct {
-            uint16_t name_index;
-        } Class;
-        struct {
-            uint16_t class_index;
-            uint16_t name_and_type_index;
-        } Fieldref;
-        struct {
-            uint16_t class_index;
-            uint16_t name_and_type_index;
-        } Methodref;
-        struct {
-            uint16_t class_index;
-            uint16_t name_and_type_index;
-        } InterfaceMethodref;
-        struct {
-            uint16_t string_index;
-        } String;
-        struct {
-            uint32_t bytes;
-        } Integer;
-        struct {
-            uint32_t bytes;
-        } Float;
-        struct {
-            uint32_t high_bytes;
-            uint32_t low_bytes;
-        } Long;
-        struct {
-            uint32_t high_bytes;
-            uint32_t low_bytes;
-        } Double;
-        struct {
-            uint16_t name_index; 
-	        uint16_t descriptor_index;
-        } NameAndType;
-        struct {
-            uint16_t length; 
-	        std::vector<uint8_t> bytes;
-        } UTF8;
-    };   
+    public:
+        uint8_t tag;
+        union {
+            struct {
+                uint16_t name_index;
+            } Class;
+            
+            struct {
+                uint16_t class_index;
+                uint16_t name_and_type_index;
+            } Fieldref;
+            
+            struct {
+                uint16_t class_index;
+                uint16_t name_and_type_index;
+            } Methodref;
+            
+            struct {
+                uint16_t class_index;
+                uint16_t name_and_type_index;
+            } InterfaceMethodref;
+            
+            struct {
+                uint16_t string_index;
+            } String;
+            
+            struct {
+                uint32_t bytes;
+            } Integer;
+            
+            struct {
+                uint32_t bytes;
+            } Float;
+            
+            struct {
+                uint32_t high_bytes;
+                uint32_t low_bytes;
+            } Long;
+            
+            struct {
+                uint32_t high_bytes;
+                uint32_t low_bytes;
+            } Double;
+            
+            struct {
+                uint16_t name_index; 
+                uint16_t descriptor_index;
+            } NameAndType;
+            
+            struct {
+                uint16_t length; 
+                std::vector<uint8_t> bytes;
+            } UTF8;
+            
+        };   
 
 };
 
