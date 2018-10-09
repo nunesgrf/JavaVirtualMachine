71beb79cf865abc4e6528ac95f3a647fad24ba83
Gabriel <nunesgrf@gmail.com>
2018-10-08 22:52:55 -0300

    printando os dados do hello_world.class

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -15,12 +15,20 @@ int main() {
     cout << "MinorVersion : " << dec << javaclass.getMinor() << endl;
     cout << "MajorVersion : " << dec << javaclass.getMajor() << endl;
     cout << "PoolCounter  : " << dec << javaclass.getConstCount() << endl;
+    cout << "AcessFlag    : " << dec << javaclass.getFlag() << endl;
+    cout << "ThisClass    : " << dec << javaclass.getThisClass() << endl;
+    cout << "SuperClass   : " << dec << javaclass.getSuper() << endl;
+    cout << "InterfaceCou : " << dec << javaclass.getInterCounter() << endl;
+    cout << "FieldsCount  : " << dec << javaclass.getFieldCount() << dec << endl;
+    cout << "MethodCount  : " << dec << javaclass.getMethoCount() << dec << endl;
+    cout << "AtributeCoun : " << dec << javaclass.getAttriCount() << endl;
+
     std::vector<CpInfo*> a = javaclass.getConstPool();
     cout << "top" << endl;
     cout << a.size() << endl;
-    //for(int i = 0; i < a.size(); i++) {
-      //  cout <<setw(2) << setfill('0') << i << " : " << setw(2) << setfill('0') << a[i]->UTF8.length << " : " << setw(2) << setfill('0') << a[i]->UTF8.bytes.size() << endl;
+    for(int i = 0; i < a.size(); i++) {
+      cout <<setw(2) << setfill('0') << i << " : " << setw(2) << setfill('0') << a[i]->UTF8.length << " : " << setw(2) << setfill('0') << a[i]->UTF8.bytes.size() << endl;
 
-    //}
+    }
     fclose(fp);
 }
