b3eaecf4afe89f21a0ac85840792d0287ed6d350
Artur Henrique <arturhenrique71@gmail.com>
2018-10-13 16:49:12 -0300

    adicionando print com molde no bytecode viewer

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -23,11 +23,39 @@ int main() {
     cout << "AtributeCoun : " << dec << classloader.getAttriCount() << endl;
 
     std::vector<CpInfo*> a = classloader.getConstPool();
-    cout << "top" << endl;
+    cout << "top" << endl << endl;
     cout << a.size() << endl;
     for(int i = 0; i < a.size(); i++) {
-      cout << setw(2) << setfill('0') << i << " : " << setw(2) << setfill('0') << a[i]->Methodref.name_and_type_index  << " : " << setw(2) << setfill('0') << 1 << endl;
+      switch (a[i]->tag){
+        case CONSTANT_Fieldref: 
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Class name = constantpool["  << a[i]->Fieldref.class_index <<"]" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Name and Type = constantpool["  << a[i]->Fieldref.name_and_type_index<<"]"  << endl <<endl;
+          break;
 
+        case CONSTANT_Methodref:
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Class name = constantpool["  << a[i]->Methodref.class_index  << "]"<< endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0')  <<"Name and Type = contantpool[" << a[i]->Methodref.name_and_type_index  << "]"<< endl << endl;
+          break;
+
+        case CONSTANT_NameAndType:
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Name = contantpool["  << a[i]->NameAndType.name_index <<"]" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Descriptor = contantpool["  << a[i]->NameAndType.descriptor_index <<"]" << endl << endl;
+          break;
+        case CONSTANT_String:
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = contantpool["  << a[i]->String.string_index << "]" << endl<<endl;
+          break;
+
+        case CONSTANT_Utf8:
+          cout << "UTF8" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Length of byte array = "  << a[i]->UTF8.length <<endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = "  << a[i]->UTF8.bytes << endl;
+          // cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = "  << a[i]->UTF8.length << endl<<endl;
+            
+        default :
+          cout << "fodase" << endl;
+
+      }
+      
     }
     fclose(fp);
 }
