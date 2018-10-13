c153de0f0008a652f4eed63a3e1e08cd57f3ec56
arturhbs <arturhenrique71@gmail.com>
2018-10-13 18:07:00 -0300

    terminando os prints constantpool

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -27,33 +27,69 @@ int main() {
     cout << a.size() << endl;
     for(int i = 0; i < a.size(); i++) {
       switch (a[i]->tag){
-        case CONSTANT_Fieldref: 
+        case CONSTANT_Fieldref:
+          cout << "Fieldref" <<endl;   
           cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Class name = constantpool["  << a[i]->Fieldref.class_index <<"]" << endl;
           cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Name and Type = constantpool["  << a[i]->Fieldref.name_and_type_index<<"]"  << endl <<endl;
           break;
 
         case CONSTANT_Methodref:
+          cout << "Methodref" <<endl;   
           cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Class name = constantpool["  << a[i]->Methodref.class_index  << "]"<< endl;
-          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0')  <<"Name and Type = contantpool[" << a[i]->Methodref.name_and_type_index  << "]"<< endl << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0')  <<"Name and Type = constantpool[" << a[i]->Methodref.name_and_type_index  << "]"<< endl << endl;
           break;
 
         case CONSTANT_NameAndType:
-          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Name = contantpool["  << a[i]->NameAndType.name_index <<"]" << endl;
-          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Descriptor = contantpool["  << a[i]->NameAndType.descriptor_index <<"]" << endl << endl;
+          cout << "NameAndType" <<endl;   
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Name = constantpool["  << a[i]->NameAndType.name_index <<"]" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Descriptor = constantpool["  << a[i]->NameAndType.descriptor_index <<"]" << endl << endl;
           break;
         case CONSTANT_String:
-          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = contantpool["  << a[i]->String.string_index << "]" << endl<<endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = constantpool["  << a[i]->String.string_index << "]" << endl<<endl;
           break;
 
         case CONSTANT_Utf8:
           cout << "UTF8" << endl;
           cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Length of byte array = "  << a[i]->UTF8.length <<endl;
-          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = "  << a[i]->UTF8.bytes << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = "  << a[i]->UTF8.bytes << endl << endl;
           // cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "String = "  << a[i]->UTF8.length << endl<<endl;
-            
-        default :
-          cout << "fodase" << endl;
+          break;
 
+        case CONSTANT_Class:
+          cout << "Class" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Class name = "  << a[i]->Class.name_index << endl << endl;
+          break;
+        
+        case CONSTANT_InterfaceMethodref:
+          cout <<  "InterfaceMethodref" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Class name = "  << a[i]->InterfaceMethodref.class_index << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Name and Type = "  << a[i]->InterfaceMethodref.name_and_type_index << endl << endl;
+          break;
+
+        case CONSTANT_Integer:
+          cout <<  "Integer" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Bytes = "  << a[i]->Integer.bytes << endl << endl;
+          break;
+
+        case CONSTANT_Float:
+          cout << "Float" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Bytes = "  << a[i]->Float.bytes << endl << endl;
+          break;
+        
+        case CONSTANT_Long:
+          cout << "Long" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "High Bytes = "  << a[i]->Long.high_bytes << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Low Bytes = "  << a[i]->Long.low_bytes << endl << endl;
+          break;
+
+        case CONSTANT_Double:
+          cout << "Double" << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "High Bytes = "  << a[i]->Double.high_bytes << endl;
+          cout << setw(2) << setfill('0') << i+1 << " : " << setw(2) << setfill('0') << "Low Bytes = "  << a[i]->Double.low_bytes << endl << endl;
+          break;  
+
+        default :
+          cout << "Non-type" << endl;
       }
       
     }
