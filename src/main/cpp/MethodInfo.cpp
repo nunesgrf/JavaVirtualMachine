0b20d4ea7ef8ddd1ee6170938788c1cc0340bfb8
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-14 21:37:03 -0300

    Colocando vector<Cpinfo> como argumento de read

--- src/main/cpp/MethodInfo.cpp
+++ src/main/cpp/MethodInfo.cpp
@@ -1,2 +1,24 @@
+#include "../hpp/MethodInfo.hpp"
+#include "ByteReader.cpp"
 
-struct MethodInfo{};
+MethodInfo::~MethodInfo()
+{
+    free(this->attributes);
+}
+
+void MethodInfo::read(FILE *fp, std::vector<CpInfo*> trueCpInfo)
+{
+    int i = 0;
+    ByteReader<uint16_t> reader;
+
+    this->acess_flags = reader.byteCatch(fp);
+    this->name_index = reader.byteCatch(fp);
+    this->descriptor_index = reader.byteCatch(fp);
+    this->attributes_count = reader.byteCatch(fp);
+
+    
+    this->attributes = (AttributeInfo *)calloc(attributes_count, sizeof(AttributeInfo));
+    for(i = 0; i < this->attributes_count; i++){
+        this->attributes[i].read(fp,trueCpInfo);
+    }
+}
