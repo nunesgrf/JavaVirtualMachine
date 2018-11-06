1b6da931d018bd3f226a6755ae274863c5286708
Gabriel <nunesgrf@gmail.com>
2018-11-06 14:33:42 -0200

    Inicio da implementação de frame

---
+++ src/main/hpp/Frame.h
@@ -0,0 +1,39 @@
+#ifndef _FRAME_H_
+#define _FRAME_H_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include "../cpp/Stack.cpp"
+
+typedef struct {
+    std::vector<Operand*> array;
+} ArrayType;
+
+struct Operand {
+    uint8_t tag;
+    union {
+        uint32_t type_bool;
+        uint32_t type_byte;
+        uint32_t type_char;
+        uint32_t type_short;
+        uint32_t type_int;
+        uint32_t type_float;
+        uint64_t type_long;
+        uint64_t type_double;
+        std::string type_string;
+        ArrayType array_type;
+    };
+};
+
+struct Frame {
+    
+    uint32_t pc; // program counter
+    Stack<Operand> operand_stack;
+    std::vector<Operand> local_variables;
+
+    void exe();
+    void setInstructs();
+};
+
+#endif
